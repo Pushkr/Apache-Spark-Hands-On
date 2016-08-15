64e77ac66b2da4436ade6cdd5da78eba07643c43
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-08-14 21:13:29 -0400

    Update LoadandSaveFiles.py

--- spark/LoadandSaveFiles.py
+++ spark/LoadandSaveFiles.py
@@ -7,6 +7,7 @@ from pyspark import SparkContext,SparkConf
 conf = SparkConf().setAppName("My app").setMaster("local")
 sc = SparkContext(conf = conf)
 
+#=======================================================================
 # load simple text file using spark
 file1 = sc.textFile("file:///home/cloudera/Desktop/pigdata/test1.txt")
 
@@ -22,6 +23,29 @@ dirFile = sc.wholeTextFiles("user/cloudera/master/part*")
 
 ListA = sc.parallelize([x for x in range(1,101)])
 ListA.saveAsTextFile("file:///home/cloudera/Desktop/outputfile")
+
+#=======================================================================
+
+
+#Loading JSON files:
+#==================
+
+# Loading json file can be accomplished using textFile() methond and using parsing meth ods 
+# like "json.loads" (for string like objects) or "json.load" (for file like object)
+#I think, json.load may not be useful since we are loading file using spark method textFile()
+# already, that gives us RDD with string like object. Need to research this more...
+
+file = sc.textFile("data.json")
+records = file.map(lambda x : json.loads(x))
+
+
+#Saving json output is reverse of loading. String like data can be parsed thru "json.dumps()"
+# and saved at end via saveAsTextFile() method
+
+records.map(lambda x : json_dumps(x))
+ .saveAsTextFile("json_output")
+ 
+ 
 
 
 
