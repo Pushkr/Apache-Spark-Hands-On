5ef8036b7307d241e58648bade4033bbd41ee44c
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-10-20 01:58:51 -0400

    Create task03.py

---
+++ exam/task03.py
@@ -0,0 +1,18 @@
+from pyspark import SparkContext,SparkConf
+
+conf = SparkConf().setAppName("Solution3")
+sc = SparkContext(conf=conf)
+
+# TO-DO : READ DATA FROM HDFS
+data = sc.       
+
+#TO-DO : CHANGE DATA FORMAT
+
+changed = data.
+
+#to-do : save data
+
+changed.
+
+sc.stop()
+
