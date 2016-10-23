32717bd8bcc74b45d1ed8b2cec8db7fec2d03591
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-10-22 22:55:50 -0400

    Update task03.py

--- exam/task03.py
+++ exam/task03.py
@@ -1,18 +1,19 @@
 from pyspark import SparkContext,SparkConf
+from pyspark.sql import HiveContext
 
 conf = SparkConf().setAppName("Solution3")
 sc = SparkContext(conf=conf)
 
+
 # TO-DO : READ DATA FROM HDFS
-data = sc.       
+data = sc.textFile("hdfs:///user/hive/warehouse/employees")       
 
 #TO-DO : CHANGE DATA FORMAT
 
-changed = data.
+changed = data.map(lambda row : row.replace(',','\t'))
 
 #to-do : save data
 
-changed.
+changed.saveAsTextFile("hdfs:///user/cloudera/solution3")
 
 sc.stop()
-
