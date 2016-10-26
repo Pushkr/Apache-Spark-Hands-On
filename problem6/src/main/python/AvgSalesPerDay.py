0e8a0f6bb79f753ee2a1a978dcba4fbbb208eebf
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-10-26 14:27:16 -0400

    Update AvgSalesPerDay.py

--- problem6/src/main/python/AvgSalesPerDay.py
+++ problem6/src/main/python/AvgSalesPerDay.py
@@ -6,20 +6,18 @@ if __name__ == '__main__':
 	sc = SparkContext(conf=conf)
 
 	#read files
-	orderFile = sc.textFile("hdfs:///user/hive/warehouse/orders")\
+	orderFile = sc.textFile("hdfs:///user/hive/warehouse/retaildb.db/orders")\
 	.filter(lambda row : row!= '')\
-	.map(lambda row : row.split("\001")) \
+	.map(lambda row : row.split(",")) \
 	.filter(lambda row: row[3] != "SUSPECT FRAUD" and row[3] != 'CANCELLED')
 
-	itemFile  = sc.textFile("hdfs:///user/hive/warehouse/order_items")\
+	itemFile  = sc.textFile("hdfs:///user/hive/warehouse/retaildb.db/order_items")\
 			.filter(lambda row : row!= '')\
-			.map(lambda row : row.split("\001"))
+			.map(lambda row : row.split(","))
 
-	orderKV = orderFile.map(lambda row:(int(row[0]),row[1][:11])) # orderId,OrderDate
-	itemKV  = itemFile.map(lambda row: (int(row[1]),float(row[4]))).reduceByKey(lambda x,y : x+y)  #orderID, totalPerOrder
+	orderKV = orderFile.map(lambda row:(int(row[0]),row[1][:11])).repartition(4) # orderId,OrderDate
+	itemKV  = itemFile.map(lambda row: (int(row[1]),float(row[4]))).reduceByKey(lambda x,y : x+y).repartition(4)  #orderID, totalPerOrder
 
-	orderKV.take(10)
-	itemKV.take(10)
 
 	joinedRDD = orderKV.join(itemKV)
 	dateRDD = joinedRDD.map(lambda row : row[1]) \
@@ -28,7 +26,7 @@ if __name__ == '__main__':
 				(lambda x,y : (x[0]+y,x[1]+1)) ,\
 				(lambda row1,row2 : (row1[0]+row2[0],row1[1]+row2[1])) \
 			)	# date, totalPerDate,totalnumberOfOrders
-	avgRDD = dateRDD.mapValues(lambda row: row[1][0]/row[1][1])
+	avgRDD = dateRDD.mapValues(lambda row: row[0]/row[1])
 	
 	avgRDD.saveAsTextFile("Avg_Sales_per_day")
 	sc.stop()
